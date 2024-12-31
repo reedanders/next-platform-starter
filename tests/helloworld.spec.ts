97eb89f16d9e7c120c0bf347f9a81dea09ed2b95
Reed Anderson <reed.forestmonster@gmail.com>
2024-12-31 06:01:00 -0800

    Update url and add wait

--- tests/helloworld.spec.ts
+++ tests/helloworld.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
+const BASE_URL = process.env.DEPLOY_URL || 'https://pedigreesync.netlify.app/';
+
 test('test', async ({ page }) => {
-  await page.goto('https://pedigreesync.netlify.app/');
+  await page.goto(BASE_URL);
   await page.getByRole('heading', { name: 'Notes Data' }).click();
 });
